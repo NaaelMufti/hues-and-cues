9c3a2ce55ff177855b2d90f496e950aa58b190c0
Naael Mufti <88817489+NaaelMufti@users.noreply.github.com>
2026-03-04 00:29:33 -0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,6 +3,10 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import { createServer as createViteServer } from "vite";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function startServer() {
   const app = express();
